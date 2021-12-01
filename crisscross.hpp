e5e5cfa91a39530eb3aac483edefc8b27c4a0ef1
nedoletoff <nedoletoff@yandex.ru>
2021-12-01 03:07:50 +0300

    Добавлен класс координат

--- crisscross.hpp
+++ crisscross.hpp
@@ -1,8 +1,24 @@
 #pragma once
 
+#include <iostream>
 #include "matrix.hpp"
 #include "words.hpp"
 #include "exceptions.hpp"
 
-void cross(Matrix& matrix, Words& words);
-bool add_word(Matrix& matrix, const std::string& word);
+struct Coordinates
+{
+  size_t h_begin;
+  size_t h_end;
+  size_t w_begin;
+  size_t w_end;
+
+  friend &ostream operator>>(ostream& os, Coordinates& value)
+  {
+    os << "begin: " << h_begin << "   " << w_begin << std::endl;
+    os << "end: " << h_end << "   " << w_end;
+    return os;
+  }
+
+};
+
+bool check_space(Coordinates& space, Matrix& matrix);
